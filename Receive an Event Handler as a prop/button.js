f26ac2084057264272d76876067202805c81afe7
Krati Chauhan <kratis9@gmail.com>
2020-08-12 11:56:10 +1000

    Receive an Event Handler as a prop

---
+++ Receive an Event Handler as a prop/button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Button extends React.Component {
+    render() {
+        return ( <
+            button onClick = {
+                this.props.onClick
+            } >
+            Click me!
+            <
+            /button>
+        );
+    }
+}
