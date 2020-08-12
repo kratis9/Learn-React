e358c4063486950cc2782a4b9a10111b639f7726
Krati Chauhan <kratis9@gmail.com>
2020-08-12 11:36:10 +1000

    eventHandler

---
+++ EventHandler/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Button extends React.Component {
+    render() {
+        return ( <
+            button >
+            Click me!
+            <
+            /button>
+        );
+    }
+}
